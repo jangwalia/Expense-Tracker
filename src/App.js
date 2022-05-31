e7fa15e33efe8571ffc7cc6a52c51136cbc30bed
jang walia <jangiwalia83@gmail.com>
2022-05-30 17:49:03 -0700

    add dynamic data to expence component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import Expense from "./Components/Expense";
 function App() {
+  const expense = [
+    { id: 1, title: "car tire", amount: 400, date: new Date(2022, 2, 15) },
+    { id: 2, title: "ymca", amount: 180, date: new Date(2022, 2, 20) },
+    { id: 2, title: "computer", amount: 800, date: new Date(2022, 2, 24) },
+    { id: 2, title: "karate", amount: 200, date: new Date(2022, 2, 28) },
+  ];
   return (
-   <Expense/>
+    <div className="expenseContainer">
+      <Expense id={expense[0].id} title={expense[0].title} amount = {expense[0].amount} date={expense[0].date} />
+      <Expense id={expense[1].id} title={expense[1].title} amount = {expense[1].amount} date={expense[1].date}/>
+      <Expense id={expense[2].id} title={expense[2].title} amount = {expense[2].amount} date={expense[2].date}/>
+      <Expense id={expense[3].id} title={expense[3].title} amount = {expense[3].amount} date={expense[3].date}/>
+    </div>
   );
 }
 
--- src/Components/Expense.jsx
+++ src/Components/Expense.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import './Expense.css'
-export default function Expense() {
+import "./Expense.css";
+export default function Expense({title,amount,date}) {
   // lets create dummy data
-  const expenseDate = new Date(2022, 4, 27);
-  const title = 'Car Tire';
-  const amount= 290.85;
+ 
   return (
-    <div className="expenseContainer">
-      <div className="dateCard">{expenseDate.toISOString()}</div>
-      <div className="details">
-        <h3>{title}</h3>
-        <p>${amount}</p>
+   <div className="expenseDetails">
+        <div className="dateCard">{date.toISOString()}</div>
+        <div className="details">
+          <h3>{title}</h3>
+          <p>${amount}</p>
+        </div>
       </div>
-    </div>
+    
   );
 }
