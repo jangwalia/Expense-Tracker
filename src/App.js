842a828d192fd639c108e8da5466ea46c4c2a41b
jang walia <jangiwalia83@gmail.com>
2022-06-10 10:12:00 -0700

    rendering list of expenses

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   const expense = [
     { id: 1, title: "car tire", amount: 400, date: new Date(2022, 2, 15) },
     { id: 2, title: "ymca", amount: 180, date: new Date(2022, 2, 20) },
-    { id: 2, title: "computer", amount: 800, date: new Date(2022, 2, 24) },
-    { id: 2, title: "karate", amount: 200, date: new Date(2022, 2, 28) },
+    { id: 3, title: "computer", amount: 800, date: new Date(2022, 2, 24) },
+    { id: 4, title: "karate", amount: 200, date: new Date(2022, 2, 28) },
   ];
   
   const handleCreateExpense = (expenses)=>{
@@ -18,7 +18,7 @@ function App() {
     <>
     <Newexpense onCreatExpense = {handleCreateExpense}/>
     <Card className="expenseContainer">
-     <ExpenseItem expense = {expense}/>
+     <ExpenseItem expenses = {expense}/>
     </Card>
     </>
     
--- src/Components/Expenses/ExpenseItem.jsx
+++ src/Components/Expenses/ExpenseItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState } from 'react';
 import Expense from './Expense'
 import FilterExpense from './FilterExpense'
-export default function ExpenseItem({expense}) {
+export default function ExpenseItem({expenses}) {
   const [filteredYear,setFilteredYear] = useState('2019');
   const handleFilter = (year) =>{
     setFilteredYear(year)
@@ -10,10 +10,15 @@ export default function ExpenseItem({expense}) {
   return (
     <div>
       <FilterExpense selected={filteredYear}  onFilterChange = {handleFilter}/>
-      <Expense  id={expense[0].id} title={expense[0].title} amount = {expense[0].amount} date={expense[0].date} />
-      <Expense  id={expense[1].id} title={expense[1].title} amount = {expense[1].amount} date={expense[1].date}/>
-      <Expense  id={expense[2].id} title={expense[2].title} amount = {expense[2].amount} date={expense[2].date}/>
-      <Expense  id={expense[3].id} title={expense[3].title} amount = {expense[3].amount} date={expense[3].date}/>
+      {expenses.map(item=>
+        <Expense
+        key = {item.id}
+        id= {item.id}
+        title = {item.title}
+        amount={item.amount}
+        date={item.date}
+        />
+      )}
     </div>
   )
 }
